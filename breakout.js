e48d0f5a7076ae3247aae2c2f69ce416d6d0f5a3
G00333093 <G00333093@gmit.ie>
2018-02-22 16:46:28 +0000

    Move the ball part 2

--- breakout.js
+++ breakout.js
@@ -7,20 +7,24 @@ var x = canvas.width/2;
 var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
+var ballRadius = 10;
 
-//draw the ball
-function draw() {
-	
+function drawBall() {
 	ctx.beginPath();
-	ctx.arc(x, y, 10, 0, Math.PI*2);
+	ctx.arc(x, y, ballRadius, 0, Math.PI*2);
 	ctx.fillStyle = "#0095DD";
 	ctx.fill();
 	ctx.closePath();
-	x+= dx;
-	x+= dy;
-	
 }
 
 
-setInterval(draw, 10);
+//draw the ball
+function draw() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	drawBall();
+	x += dx;
+	y += dy;
+	
+}
 
+setInterval(draw, 10);
