1747a8c76b2a230664fb28a9c85ca827a1bdf751
G00333093 <G00333093@gmit.ie>
2018-02-22 16:12:45 +0000

    Move the ball

--- breakout.js
+++ breakout.js
@@ -1,26 +1,26 @@
+//set up the canvas
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
-ctx.beginPath();
-ctx.rect(20, 40, 100, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+//set the starting point
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = 2;
+var dy = -2;
 
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI*2, false);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
+//draw the ball
+function draw() {
+	
+	ctx.beginPath();
+	ctx.arc(x, y, 10, 0, Math.PI*2);
+	ctx.fillStyle = "#0095DD";
+	ctx.fill();
+	ctx.closePath();
+	x+= dx;
+	x+= dy;
+	
+}
 
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-ctx.stroke();
-ctx.closePath();
 
-ctx.beginPath();
-ctx.rect(450, 290, 20, 20);
-ctx.fillStyle = "yellow";
-ctx.fill();
-ctx.closePath();
+setInterval(draw, 10);
+
